7e3dc9db0f8c45ecc42499d91c052e50e28609d9
lucas goeten <lucasgoetengithub@gmail.com>
2022-06-15 21:40:17 -0300

    change status customer to inactive

--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.enums.CustomerStatus
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.repository.CustomerRepository
 import org.springframework.stereotype.Service
@@ -36,6 +37,7 @@ class CustomerService(
     fun deleteCustomer(@PathVariable id:Int) {
         val customer = findById(id)
         bookService.deleteByCustomer(customer)
-        customerRepository.deleteById(id)
+        customer.status = CustomerStatus.INATIVO;
+        customerRepository.save(customer)
     }
 }
