40680182e4fff2c22f04431d3c2628dd9e05185b
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-07-03 13:01:10 -0300

    adicinonando validação de erros em outros endpoints

--- src/main/kotlin/com/mercadolivro/controller/request/PostBookRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PostBookRequest.kt
@@ -2,12 +2,16 @@ package com.mercadolivro.controller.request
 
 import com.fasterxml.jackson.annotation.JsonAlias
 import java.math.BigDecimal
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
-data class PostBookRequest (
-    var name: String,
+data class PostBookRequest(
+        @field:NotEmpty(message = "Nome deve ser informado.")
+        var name: String,
 
-    var price: BigDecimal,
+        @field:NotNull(message = "Preço deve ser informado.")
+        var price: BigDecimal,
 
-    @JsonAlias("customer_id")
-    var customerId: Int
+        @JsonAlias("customer_id")
+        var customerId: Int
 )
