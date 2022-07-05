8faa16d98533f33fe690643e61edc3102bb0ceb4
Lucas Goeten <lucasgoetengithub@gmail.com>
2022-07-05 00:13:53 -0300

    swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.0"
+	id("org.springframework.boot") version "2.6.7"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
@@ -12,6 +12,9 @@ group = "com.mercadolivro"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+val springDocVersion = "1.6.8"
+val flywayVersion = "8.5.10"
+
 repositories {
 	mavenCentral()
 }
@@ -21,14 +24,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.0")
 	implementation("org.springframework.boot:spring-boot-starter-validation:2.7.1")
 
-	implementation("io.springfox:springfox-swagger2:2.9.2")
-	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+	implementation("org.springdoc:springdoc-openapi-ui:$springDocVersion")
+	implementation("org.springdoc:springdoc-openapi-kotlin:$springDocVersion")
 
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.flywaydb:flyway-core:8.5.12")
-	implementation("org.flywaydb:flyway-mysql")
+	implementation("org.flywaydb:flyway-core:$flywayVersion")
+	implementation("org.flywaydb:flyway-mysql:$flywayVersion")
 
 
 	runtimeOnly("mysql:mysql-connector-java")
---
+++ src/main/kotlin/com/mercadolivro/config/OpenApiConfig.kt
@@ -0,0 +1,20 @@
+package com.mercadolivro.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+@Profile("!prod")
+@Configuration
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        return OpenAPI().info(
+                Info().title("MercadoLivro API")
+                        .description("This API provides resources for MercadoLivro.")
+        )
+    }
+}
